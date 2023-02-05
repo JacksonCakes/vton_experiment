3d8ee5670a30f4c8b611f84c8ebb2424864255b9
JacksonCakes <jacksonkek257@gmail.com>
2023-02-05 23:21:26 +0800

    first commit

---
+++ train/models/networks.py
@@ -0,0 +1,316 @@
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+from torchvision import models
+from options.train_options import TrainOptions
+import os
+import torch.nn.functional as F
+opt = TrainOptions().parse()
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features=64, norm_layer=nn.BatchNorm2d):
+        super(ResidualBlock, self).__init__()
+        self.relu = nn.ReLU(True)
+        if norm_layer == None:
+            self.block = nn.Sequential(
+                nn.Conv2d(in_features, in_features, 3, 1, 1, bias=False),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(in_features, in_features, 3, 1, 1, bias=False),
+            )
+        else:
+            self.block = nn.Sequential(
+                nn.Conv2d(in_features, in_features, 3, 1, 1, bias=False),
+                norm_layer(in_features),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(in_features, in_features, 3, 1, 1, bias=False),
+                norm_layer(in_features)
+            )
+
+    def forward(self, x):
+        residual = x
+        out = self.block(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+
+class ResUnetGenerator(nn.Module):
+    def __init__(self, input_nc, output_nc, num_downs, ngf=64,
+                 norm_layer=nn.BatchNorm2d, use_dropout=False):
+        super(ResUnetGenerator, self).__init__()
+        # construct unet structure
+        unet_block = ResUnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=None, norm_layer=norm_layer, innermost=True)
+
+        for i in range(num_downs - 5):
+            unet_block = ResUnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer, use_dropout=use_dropout)
+        unet_block = ResUnetSkipConnectionBlock(ngf * 4, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
+        unet_block = ResUnetSkipConnectionBlock(ngf * 2, ngf * 4, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
+        unet_block = ResUnetSkipConnectionBlock(ngf, ngf * 2, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
+        unet_block = ResUnetSkipConnectionBlock(output_nc, ngf, input_nc=input_nc, submodule=unet_block, outermost=True, norm_layer=norm_layer)
+
+        self.model = unet_block
+        self.old_lr = opt.lr
+        self.old_lr_gmm = 0.1*opt.lr
+
+    def forward(self, input):
+        return self.model(input)
+
+
+# Defines the submodule with skip connection.
+# X -------------------identity---------------------- X
+#   |-- downsampling -- |submodule| -- upsampling --|
+class ResUnetSkipConnectionBlock(nn.Module):
+    def __init__(self, outer_nc, inner_nc, input_nc=None,
+                 submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False):
+        super(ResUnetSkipConnectionBlock, self).__init__()
+        self.outermost = outermost
+        use_bias = norm_layer == nn.InstanceNorm2d
+
+        if input_nc is None:
+            input_nc = outer_nc
+        downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=3,
+                             stride=2, padding=1, bias=use_bias)
+        # add two resblock
+        res_downconv = [ResidualBlock(inner_nc, norm_layer), ResidualBlock(inner_nc, norm_layer)]
+        res_upconv = [ResidualBlock(outer_nc, norm_layer), ResidualBlock(outer_nc, norm_layer)]
+
+        downrelu = nn.ReLU(True)
+        uprelu = nn.ReLU(True)
+        if norm_layer != None:
+            downnorm = norm_layer(inner_nc)
+            upnorm = norm_layer(outer_nc)
+
+        if outermost:
+            upsample = nn.Upsample(scale_factor=2, mode='nearest')
+            upconv = nn.Conv2d(inner_nc * 2, outer_nc, kernel_size=3, stride=1, padding=1, bias=use_bias)
+            down = [downconv, downrelu] + res_downconv
+            up = [upsample, upconv]
+            model = down + [submodule] + up
+        elif innermost:
+            upsample = nn.Upsample(scale_factor=2, mode='nearest')
+            upconv = nn.Conv2d(inner_nc, outer_nc, kernel_size=3, stride=1, padding=1, bias=use_bias)
+            down = [downconv, downrelu] + res_downconv
+            if norm_layer == None:
+                up = [upsample, upconv, uprelu] + res_upconv
+            else:
+                up = [upsample, upconv, upnorm, uprelu] + res_upconv
+            model = down + up
+        else:
+            upsample = nn.Upsample(scale_factor=2, mode='nearest')
+            upconv = nn.Conv2d(inner_nc*2, outer_nc, kernel_size=3, stride=1, padding=1, bias=use_bias)
+            if norm_layer == None:
+                down = [downconv, downrelu] + res_downconv
+                up = [upsample, upconv, uprelu] + res_upconv
+            else:
+                down = [downconv, downnorm, downrelu] + res_downconv
+                up = [upsample, upconv, upnorm, uprelu] + res_upconv
+
+            if use_dropout:
+                model = down + [submodule] + up + [nn.Dropout(0.5)]
+            else:
+                model = down + [submodule] + up
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        if self.outermost:
+            return self.model(x)
+        else:
+            return torch.cat([x, self.model(x)], 1)
+
+
+class Vgg19(nn.Module):
+    def __init__(self, requires_grad=False):
+        super(Vgg19, self).__init__()
+        vgg_pretrained_features = models.vgg19(pretrained=True).features
+        self.slice1 = nn.Sequential()
+        self.slice2 = nn.Sequential()
+        self.slice3 = nn.Sequential()
+        self.slice4 = nn.Sequential()
+        self.slice5 = nn.Sequential()
+        for x in range(2):
+            self.slice1.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(2, 7):
+            self.slice2.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(7, 12):
+            self.slice3.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(12, 21):
+            self.slice4.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(21, 30):
+            if isinstance(vgg_pretrained_features[x],nn.MaxPool2d):
+                vgg_pretrained_features[x].ceil_mode = True
+            self.slice5.add_module(str(x), vgg_pretrained_features[x])
+        if not requires_grad:
+            for param in self.parameters():
+                param.requires_grad = False
+
+    def forward(self, X):
+        h_relu1 = self.slice1(X)
+        h_relu2 = self.slice2(h_relu1)
+        h_relu3 = self.slice3(h_relu2)
+        h_relu4 = self.slice4(h_relu3)
+        h_relu5 = self.slice5(h_relu4)
+        out = [h_relu1, h_relu2, h_relu3, h_relu4, h_relu5]
+        return out
+
+class VGGLoss(nn.Module):
+    def __init__(self, layids = None):
+        super(VGGLoss, self).__init__()
+        self.vgg = Vgg19()
+        self.vgg.cuda()
+        self.criterion = nn.L1Loss()
+        self.weights = [1.0/32, 1.0/16, 1.0/8, 1.0/4, 1.0]
+        self.layids = layids
+
+    def forward(self, x, y):
+        x_vgg, y_vgg = self.vgg(x), self.vgg(y)
+        loss = 0
+        if self.layids is None:
+            self.layids = list(range(len(x_vgg)))
+        for i in self.layids:
+            loss += self.weights[i] * self.criterion(x_vgg[i], y_vgg[i].detach())
+        return loss
+
+
+def gauss_kernel(size=5, device=torch.device('cpu'), channels=3):
+    kernel = torch.tensor([[1., 4., 6., 4., 1],
+                           [4., 16., 24., 16., 4.],
+                           [6., 24., 36., 24., 6.],
+                           [4., 16., 24., 16., 4.],
+                           [1., 4., 6., 4., 1.]])
+    kernel /= 256.
+    kernel = kernel.repeat(channels, 1, 1, 1)
+    kernel = kernel.to(device)
+    return kernel
+
+def downsample(x):
+    return x[:, :, ::2, ::2]
+
+def upsample(x):
+    cc = torch.cat([x, torch.zeros(x.shape[0], x.shape[1], x.shape[2], x.shape[3], device=x.device)], dim=3)
+    cc = cc.view(x.shape[0], x.shape[1], x.shape[2]*2, x.shape[3])
+    cc = cc.permute(0,1,3,2)
+    cc = torch.cat([cc, torch.zeros(x.shape[0], x.shape[1], x.shape[3], x.shape[2]*2, device=x.device)], dim=3)
+    cc = cc.view(x.shape[0], x.shape[1], x.shape[3]*2, x.shape[2]*2)
+    x_up = cc.permute(0,1,3,2)
+    return conv_gauss(x_up, 4*gauss_kernel(channels=x.shape[1], device=x.device))
+
+def conv_gauss(img, kernel):
+    img = torch.nn.functional.pad(img, (2, 2, 2, 2), mode='reflect')
+    out = torch.nn.functional.conv2d(img, kernel, groups=img.shape[1])
+    return out
+
+def laplacian_pyramid(img, kernel, max_levels=3):
+    current = img
+    pyr = []
+    for level in range(max_levels):
+        filtered = conv_gauss(current, kernel)
+        down = downsample(filtered)
+        up = upsample(down)
+        diff = current-up
+        pyr.append(diff)
+        current = down
+    return pyr
+
+class LapLoss(torch.nn.Module):
+    def __init__(self, max_levels=3, channels=3, device=torch.device('cpu')):
+        super(LapLoss, self).__init__()
+        self.max_levels = max_levels
+        self.gauss_kernel = gauss_kernel(channels=channels, device=device)
+        
+    def forward(self, input, target):
+        pyr_input  = laplacian_pyramid(img=input, kernel=self.gauss_kernel, max_levels=self.max_levels)
+        pyr_target = laplacian_pyramid(img=target, kernel=self.gauss_kernel, max_levels=self.max_levels)
+        return sum(torch.nn.functional.l1_loss(a, b) for a, b in zip(pyr_input, pyr_target))
+
+class SobelLoss(torch.nn.Module):
+    def __init__(self,device):
+        super(SobelLoss,self).__init__()
+        self.sobel_x = torch.tensor([[-1,-2,-1],[0,0,0],[1,2,1]],requires_grad=False).view(1,1,3,3).repeat(1,3,1,1).to(device)
+        self.sobely = torch.tensor([[-1,0,1],[-2,0,2],[-1,0,1]],requires_grad=False).view(1,1,3,3).to(device)
+    
+    def forward(self,img,target):
+        img_x = F.conv2d(img,self.sobel_x,stride=1,padding=1)
+        img_y = F.conv2d(img,self.sobel_y,stride=1,padding=1)
+        target_x = F.conv2d(target,self.sobel_x,stride=1,padding=1)
+        target_y = F.conv2d(target,self.sobel_y,stride=1,padding=1)
+        loss = F.l1_loss(img_x,target_x)+F.l1_loss(img_y,target_y)
+        return img_x,img_y,target_x,target_y,loss
+
+class GradLayer(nn.Module):
+
+    def __init__(self,device):
+        super(GradLayer, self).__init__()
+        kernel_v = [[0, -1, 0],
+                    [0, 0, 0],
+                    [0, 1, 0]]
+        kernel_h = [[0, 0, 0],
+                    [-1, 0, 1],
+                    [0, 0, 0]]
+        kernel_h = torch.FloatTensor(kernel_h).unsqueeze(0).unsqueeze(0)
+        kernel_v = torch.FloatTensor(kernel_v).unsqueeze(0).unsqueeze(0)
+        self.weight_h = nn.Parameter(data=kernel_h, requires_grad=False).to(device)
+        self.weight_v = nn.Parameter(data=kernel_v, requires_grad=False).to(device)
+
+    def get_gray(self,x):
+        '''
+        Convert image to its gray one.
+        '''
+        gray_coeffs = [65.738, 129.057, 25.064]
+        convert = x.new_tensor(gray_coeffs).view(1, 3, 1, 1) / 256
+        x_gray = x.mul(convert).sum(dim=1)
+        return x_gray.unsqueeze(1)
+
+    def forward(self, x):
+        if x.shape[1] == 3:
+            x = self.get_gray(x)
+
+        x_v = F.conv2d(x, self.weight_v, padding=1)
+        x_h = F.conv2d(x, self.weight_h, padding=1)
+        x = torch.sqrt(torch.pow(x_v, 2) + torch.pow(x_h, 2) + 1e-6)
+
+        return x_v,x_h,x
+
+class GradLoss(nn.Module):
+
+    def __init__(self,device):
+        super(GradLoss, self).__init__()
+        self.loss = nn.L1Loss()
+        self.grad_layer = GradLayer(device)
+
+    def forward(self, output, gt_img):
+        v,h,output_grad = self.grad_layer(output)
+        gt_v,gt_h,gt_grad = self.grad_layer(gt_img)
+        return self.loss(output_grad, gt_grad),v,h,gt_v,gt_h
+
+def save_checkpoint(model, save_path):
+    if not os.path.exists(os.path.dirname(save_path)):
+        os.makedirs(os.path.dirname(save_path))
+    torch.save(model.state_dict(), save_path)
+
+
+def load_checkpoint_parallel(model, checkpoint_path):
+
+    if not os.path.exists(checkpoint_path):
+        print('No checkpoint!')
+        return
+
+    checkpoint = torch.load(checkpoint_path, map_location='cuda:{}'.format(opt.local_rank))
+    checkpoint_new = model.state_dict()
+    for param in checkpoint_new:
+        checkpoint_new[param] = checkpoint[param]
+    model.load_state_dict(checkpoint_new)
+
+def load_checkpoint_part_parallel(model, checkpoint_path):
+
+    if not os.path.exists(checkpoint_path):
+        print('No checkpoint!')
+        return
+    checkpoint = torch.load(checkpoint_path,map_location='cuda:{}'.format(opt.local_rank))
+    checkpoint_new = model.state_dict()
+    for param in checkpoint_new:
+        if 'cond_' not in param and 'aflow_net.netRefine' not in param or 'aflow_net.cond_style' in param:
+            checkpoint_new[param] = checkpoint[param]
+    model.load_state_dict(checkpoint_new)
+
+
---
+++ train/train_ml.py
@@ -0,0 +1,329 @@
+import time
+from options.train_options import TrainOptions
+from models.networks import ResUnetGenerator, VGGLoss, save_checkpoint, load_checkpoint_parallel
+from models.afwm import TVLoss, AFWM
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+from tensorboardX import SummaryWriter
+import cv2
+import datetime
+from util import flow_util
+from util import util
+
+
+def de_offset(s_grid):
+    [b, _, h, w] = s_grid.size()
+
+    x = torch.arange(w).view(1, -1).expand(h, -1).float()
+    y = torch.arange(h).view(-1, 1).expand(-1, w).float()
+    x = 2 * x / (w - 1) - 1
+    y = 2 * y / (h - 1) - 1
+    grid = torch.stack([x, y], dim=0).float().cuda()
+    grid = grid.unsqueeze(0).expand(b, -1, -1, -1)
+
+    offset = grid - s_grid
+
+    offset_x = offset[:, 0, :, :] * (w - 1) / 2
+    offset_y = offset[:, 1, :, :] * (h - 1) / 2
+
+    offset = torch.cat((offset_y, offset_x), 0)
+
+    return offset
+
+def eval_delta_loss(delta_x, epsilon):
+    delta_loss = (delta_x.pow(2) + epsilon * epsilon).pow(0.45)
+    return delta_loss
+    
+f2c = flow_util.flow2color()
+opt = TrainOptions().parse()
+path = 'runs/' + opt.name
+os.makedirs(path, exist_ok=True)
+os.makedirs(opt.checkpoints_dir, exist_ok=True)
+torch.backends.cudnn.benchmark = True
+
+
+def CreateDataset(opt):
+    # training with augumentation
+    # from data.aligned_dataset import AlignedDataset_aug
+    # dataset = AlignedDataset_aug()
+    from data.aligned_dataset import AlignedDataset
+    dataset = AlignedDataset()
+    print("dataset [%s] was created" % (dataset.name()))
+    dataset.initialize(opt)
+    return dataset
+
+
+os.makedirs('sample_fs', exist_ok=True)
+iter_path = os.path.join(opt.checkpoints_dir, opt.name, 'iter.txt')
+
+torch.cuda.set_device(opt.local_rank)
+torch.distributed.init_process_group(
+    'nccl',
+    init_method='env://'
+)
+device = torch.device(f'cuda:{opt.local_rank}')
+
+start_epoch, epoch_iter = 1, 0
+
+train_data = CreateDataset(opt)
+train_sampler = DistributedSampler(train_data)
+train_loader = DataLoader(train_data, batch_size=opt.batchSize, shuffle=False,
+                          num_workers=4, pin_memory=True, sampler=train_sampler)
+dataset_size = len(train_loader)
+
+PBTN = AFWM(opt, 3)
+PFSN = AFWM(opt, 3)
+
+PBTN.train()
+PBTN.cuda()
+PBTN = torch.nn.SyncBatchNorm.convert_sync_batchnorm(PBTN).to(device)
+#load_checkpoint_parallel(PBTN, opt.PBTN_checkpoint)
+
+PFSN.train()
+PFSN.cuda()
+PFSN = torch.nn.SyncBatchNorm.convert_sync_batchnorm(PFSN).to(device)
+#load_checkpoint_parallel(PFSN, opt.PFSN_checkpoint)
+
+gen_model = ResUnetGenerator(7, 4, 5, ngf=64, norm_layer=nn.BatchNorm2d)
+gen_model.eval()
+gen_model.cuda()
+load_checkpoint_parallel(gen_model, opt.gen_checkpoint)
+
+if opt.isTrain and len(opt.gpu_ids):
+    teacher_model = torch.nn.parallel.DistributedDataParallel(PBTN, device_ids=[opt.local_rank])
+    stud_model = torch.nn.parallel.DistributedDataParallel(PFSN, device_ids=[opt.local_rank])
+    gen_model = torch.nn.parallel.DistributedDataParallel(gen_model, device_ids=[opt.local_rank])
+
+criterionL1 = nn.L1Loss()
+criterionVGG = VGGLoss()
+
+params_warp_tea = [p for p in teacher_model.parameters()]
+params_warp_stud = [p for p in stud_model.parameters()]
+optimizer_warp_tea = torch.optim.Adam(params_warp_tea, lr=opt.lr, betas=(opt.beta1, 0.999))
+optimizer_warp_stud = torch.optim.Adam(params_warp_stud, lr=opt.lr, betas=(opt.beta1, 0.999))
+
+total_steps = (start_epoch - 1) * dataset_size + epoch_iter
+step = 1
+step_per_batch = dataset_size
+
+if opt.local_rank == 0:
+    writer = SummaryWriter(path)
+
+for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
+    epoch_start_time = time.time()
+    if epoch != start_epoch:
+        epoch_iter = epoch_iter % dataset_size
+    start_time = time.time()
+    train_sampler.set_epoch(epoch) # to make shuffling work properly across multiple epochs
+
+    for i, data in enumerate(train_loader):
+        iter_start_time = time.time()
+
+        total_steps += 1
+        epoch_iter += 1
+        save_fake = True
+
+        t_mask = torch.FloatTensor((data['label'].cpu().numpy() == 7).astype(np.float64))
+        data['label'] = data['label'] * (1 - t_mask) + t_mask * 4
+        parsing_result,person_clothes,densepose_fore,clothes_un,pre_clothes_edge_un, clothes,pre_clothes_edge,real_image,person_clothes_edge  = util.prepare_data(data)
+        # import ipdb; ipdb.set_trace()
+        with torch.no_grad():
+            flow_out_int = teacher_model(real_image.cuda(), clothes_un.cuda(), pre_clothes_edge_un.cuda())
+            warped_cloth_int, last_flow_int, _1, _2, _3, x_all_int, x_edge_all_int, _5, _6 = flow_out_int
+            warped_prod_edge_int = x_edge_all_int[4]
+
+            gen_inputs_int = torch.cat([real_image.cuda(), warped_cloth_int, warped_prod_edge_int], 1)
+            gen_outputs_int = gen_model(gen_inputs_int)
+            p_rendered_int, m_composite_int = torch.split(gen_outputs_int, [3, 1], 1)
+            p_rendered_int = torch.tanh(p_rendered_int)
+            m_composite_int = torch.sigmoid(m_composite_int)
+            m_composite_int = m_composite_int * warped_prod_edge_int
+            p_tryon_int = warped_cloth_int * m_composite_int + p_rendered_int * (1 - m_composite_int)
+
+        flow_out = teacher_model(real_image.cuda(), clothes.cuda(), pre_clothes_edge.cuda())
+        warped_cloth, last_flow, cond_all, flow_all, delta_list, x_all, x_edge_all, delta_x_all, delta_y_all = flow_out
+        warped_prod_edge = x_edge_all[4]
+
+        flow_out_stud = stud_model(p_tryon_int.detach(), clothes.cuda(), pre_clothes_edge.cuda())
+        warped_cloth_stud, last_flow_stud, cond_all_stud, flow_all_stud, delta_list_stud, x_all_stud, x_edge_all_stud, delta_x_all_stud, delta_y_all_stud = flow_out_stud
+        warped_prod_edge_stud = x_edge_all_stud[4]
+
+        epsilon = 0.001
+        loss_smooth_tea = sum([TVLoss(x) for x in delta_list])
+        loss_smooth_stud = sum([TVLoss(x) for x in delta_list_stud])
+        loss_smooth_total = loss_smooth_tea + loss_smooth_stud
+        # print('Loss smooth total: ',loss_smooth_total)
+        loss_all = 0
+        loss_tea = 0
+        loss_stud = 0
+        l1_loss_batch = torch.abs(warped_cloth.detach() - person_clothes.cuda())
+        l1_loss_batch = l1_loss_batch.reshape(opt.batchSize, 3 * 256 * 192)
+        l1_loss_batch = l1_loss_batch.sum(dim=1) / (3 * 256 * 192)
+        l1_loss_batch_pred = torch.abs(warped_cloth_stud.detach() - person_clothes.cuda())
+        l1_loss_batch_pred = l1_loss_batch_pred.reshape(opt.batchSize, 3 * 256 * 192)
+        l1_loss_batch_pred = l1_loss_batch_pred.sum(dim=1) / (3 * 256 * 192)
+        weight = (l1_loss_batch < l1_loss_batch_pred).float()
+        num_all = len(np.where(weight.cpu().numpy() > 0)[0])
+       
+        # print('Num all: ',num_all)
+        if num_all == 0:
+            num_all = 1
+    
+
+        for num in range(5):
+            cur_person_clothes = F.interpolate(person_clothes, scale_factor=0.5 ** (4 - num), mode='bilinear')
+            cur_person_clothes_edge = F.interpolate(person_clothes_edge, scale_factor=0.5 ** (4 - num), mode='bilinear')
+            loss_l1 = criterionL1(x_all[num], cur_person_clothes.cuda())
+            loss_vgg = criterionVGG(x_all[num], cur_person_clothes.cuda())
+            loss_edge = criterionL1(x_edge_all[num], cur_person_clothes_edge.cuda())
+            b, c, h, w = delta_x_all[num].shape
+            loss_flow_x = eval_delta_loss(delta_x_all[num], epsilon)
+            loss_flow_x = torch.sum(loss_flow_x) / (b * c * h * w)
+            loss_flow_y = eval_delta_loss(delta_y_all[num], epsilon)
+            loss_flow_y = torch.sum(loss_flow_y) / (b * c * h * w)
+            loss_second_smooth = loss_flow_x + loss_flow_y
+            b1, c1, h1, w1 = cond_all[num].shape
+            
+            loss_tea = loss_tea + (num + 1) * loss_l1 + (num + 1) * 0.2 * loss_vgg + (num + 1) * 2 * loss_edge + (
+                        num + 1) * 6 * loss_second_smooth 
+
+            loss_l1_stud = criterionL1(x_all_stud[num], cur_person_clothes.cuda())
+            loss_vgg_stud = criterionVGG(x_all_stud[num], cur_person_clothes.cuda())
+            loss_edge_stud = criterionL1(x_edge_all_stud[num], cur_person_clothes_edge.cuda())
+
+            loss_flow_x_stud = eval_delta_loss(delta_x_all_stud[num], epsilon)
+            loss_flow_x_stud = torch.sum(loss_flow_x_stud) / (b * c * h * w)
+            loss_flow_y_stud = eval_delta_loss(delta_y_all_stud[num], epsilon)
+            loss_flow_y_stud = torch.sum(loss_flow_y_stud) / (b * c * h * w)
+            loss_second_smooth_stud = loss_flow_x_stud + loss_flow_y_stud
+            
+            weight_all = weight.reshape(-1, 1, 1, 1).repeat(1, 256, h1, w1)
+            cond_sup_loss = ((cond_all[num].detach() - cond_all_stud[num]) ** 2 * weight_all).sum() / (256 * h1 * w1 * num_all)
+         
+            loss_stud = loss_stud + (num + 1) * loss_l1_stud + (num + 1) * 0.2 * loss_vgg_stud + (
+                        num + 1) * 2 * loss_edge_stud + (num + 1) * 6 * loss_second_smooth_stud + cond_sup_loss
+
+
+            if num >= 2:
+                b1, c1, h1, w1 = flow_all_stud[num].shape
+                weight_flow_stud = weight.reshape(-1, 1, 1).repeat(1, h1, w1)
+                flow_loss_stud = (torch.norm(flow_all[num].detach() - flow_all_stud[num], p=2,
+                                             dim=1) * weight_flow_stud).sum() / (h1 * w1 * num_all)
+                loss_stud = loss_stud + (num + 1) * 1 * flow_loss_stud
+                # print('Flow loss-{}: {}'.format(num,flow_loss_stud))
+
+        loss_all = 0.01 * loss_smooth_total + loss_tea + loss_stud
+
+        if opt.local_rank == 0:
+            writer.add_scalar('loss_all', loss_all, step)
+
+        optimizer_warp_tea.zero_grad(set_to_none=True)
+        optimizer_warp_stud.zero_grad(set_to_none=True)
+        loss_all.backward()
+        optimizer_warp_tea.step()
+        optimizer_warp_stud.step()
+        ############## Display results and errors ##########
+
+        path = opt.name
+        os.makedirs(path, exist_ok=True)
+        if step % 1000 == 0:
+            if opt.local_rank == 0:
+                a = real_image.float().cuda()
+                b = person_clothes.cuda()  # GT
+                c = clothes.cuda()
+                c_un = clothes_un.cuda()
+                d = torch.cat([densepose_fore.cuda(), densepose_fore.cuda(), densepose_fore.cuda()], 1)
+                e = warped_cloth
+                e_stu = warped_cloth_stud
+                e_un = warped_cloth_int
+                p = p_tryon_int
+                flow_offset = de_offset(last_flow[0].unsqueeze(0))
+                flow_color = f2c(flow_offset).cuda()
+                flow_offset_int = de_offset(last_flow_int[0].unsqueeze(0))
+                flow_color_int = f2c(flow_offset_int).cuda()
+                flow_offset_stud = de_offset(last_flow_stud[0].unsqueeze(0))
+                flow_color_stud = f2c(flow_offset_stud).cuda()
+
+                combine = torch.cat(
+                    [a[0], b[0], c[0], c_un[0], d[0], e[0], e_stu[0], e_un[0], p[0], flow_color, flow_color_stud,
+                     flow_color_int], 2).squeeze()
+                cv_img = (combine.permute(1, 2, 0).detach().cpu().numpy() + 1) / 2
+                writer.add_image('combine', (combine.data + 1) / 2.0, step)
+                rgb = (cv_img * 255).astype(np.uint8)
+                bgr = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+                cv2.imwrite(path + '/' + str(step) + '.jpg', bgr)
+
+        step += 1
+        iter_end_time = time.time()
+        iter_delta_time = iter_end_time - iter_start_time
+        step_delta = (step_per_batch - step % step_per_batch) + step_per_batch * (opt.niter + opt.niter_decay - epoch)
+        eta = iter_delta_time * step_delta
+        eta = str(datetime.timedelta(seconds=int(eta)))
+        time_stamp = datetime.datetime.now()
+        now = time_stamp.strftime('%Y.%m.%d-%H:%M:%S')
+        if step % 100 == 0:
+            if opt.local_rank == 0:
+                end_time = time.time()
+                elapsed_time = end_time - start_time
+                print('{}:{}:[step-{}]--[loss total-{:.6f}]--[loss teacher-{}]--[loss_student-{}]--[ETA-{}]'.format(now, epoch_iter,step, loss_all,loss_tea,loss_stud,eta))
+
+        if epoch_iter >= dataset_size:
+            break
+
+    # end of epoch
+    iter_end_time = time.time()
+    if opt.local_rank == 0:
+        print('End of epoch %d / %d \t Time Taken: %d sec' %
+              (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+
+    ### save model for this epoch
+    if epoch % opt.save_epoch_freq == 0:
+        if opt.local_rank == 0:
+            print('saving the model at the end of epoch %d, iters %d' % (epoch, total_steps))
+            save_checkpoint(teacher_model.module,
+                            os.path.join(opt.checkpoints_dir, opt.name, 'teacher_warp_epoch_%03d.pth' % (epoch + 1)))
+            save_checkpoint(stud_model.module,
+                            os.path.join(opt.checkpoints_dir, opt.name, 'student_warp_epoch_%03d.pth' % (epoch + 1)))
+
+    if epoch > opt.niter:
+        teacher_model.module.update_learning_rate(optimizer_warp_tea)
+        stud_model.module.update_learning_rate(optimizer_warp_stud)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
